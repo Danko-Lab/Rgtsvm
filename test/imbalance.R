ca4a3881ffae22b40de73da9b5d03860d36960da
ZHONG WANG <tg826494@login3.stampede.tacc.utexas.edu>
2016-04-08 13:20:54 -0500

    all test files

---
+++ test/imbalance.R
@@ -0,0 +1,17 @@
+library(ROCR)
+library(caTools)
+
+auc_pr <- function(obs, pred) {
+  xx.df <- prediction(pred, obs)
+  perf  <- performance(xx.df, "prec", "rec")
+  xy    <- data.frame(recall=perf@x.values[[1]], precision=perf@y.values[[1]])
+  
+  # take out division by 0 for lowest threshold
+  xy <- subset(xy, !is.nan(xy$precision))
+  
+  res   <- trapz(xy$recall, xy$precision)
+  res
+}
+
+dnase_prauc=auc_pr(statusDNASE, predDNASE)
+
