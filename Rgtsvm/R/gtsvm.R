8bae656e69981fcd5cdfcf752781d637158e786b
Zhong Wang <zw355@cbsugpu01.tc.cornell.edu>
2017-06-10 15:33:34 -0400

    Appending the GPL-v3 license information

--- Rgtsvm/R/gtsvm.R
+++ Rgtsvm/R/gtsvm.R
@@ -1,3 +1,20 @@
+##
+##	Copyright (C) 2017  Zhong Wang
+##
+##	This program is free software: you can redistribute it and/or modify
+##	it under the terms of the GNU General Public License as published by
+##	the Free Software Foundation, either version 3 of the License, or
+##	(at your option) any later version.
+##
+##	This program is distributed in the hope that it will be useful,
+##	but WITHOUT ANY WARRANTY; without even the implied warranty of
+##	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##	GNU General Public License for more details.
+##
+##	You should have received a copy of the GNU General Public License
+##	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 trim.space <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 gtsvmtrain.classfication.call<-function(y, x, param, final.result=FALSE, verbose=FALSE, ignoreNoProgress=FALSE)
--- Rgtsvm/src/svm.hpp
+++ Rgtsvm/src/svm.hpp
@@ -1,5 +1,6 @@
 /*
 	Copyright (C) 2011  Andrew Cotter
+	Copyright (C) 2017  Zhong Wang
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
